e361f369561e5e064d2bcdf7fffe5fec58f92df8
Thuy.Copeland <thuycopeland@gmail.com>
2020-05-20 17:15:18 -0500

    Create a SiteAddressViewController

---
+++ WordPressAuthenticator/Unified Auth/View Related/SiteAddressViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+
+/// SiteAddressViewController: Displays the site address field to log in.
+///
+class SiteAddressViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
