1b7e52b51bdd6c85aa685867eb4ea3f0b901ae67
Thuy.Copeland <thuycopeland@gmail.com>
2020-05-20 17:45:31 -0500

    Getting too far ahead. Let's define 1 method in each protocol.

--- WordPressAuthenticator/Unified Auth/Classes/ViewControllerFactory.swift
+++ WordPressAuthenticator/Unified Auth/Classes/ViewControllerFactory.swift
@@ -3,10 +3,8 @@ import Foundation
 
 protocol ViewControllerFactory {
     func makeSiteAddressViewController() -> SiteAddressViewController
-    func makeUsernamePasswordViewController() -> LoginUsernamePasswordViewController
 }
 
 protocol LoginFieldLoaderFactory {
     func makeLoginFieldLoader() -> LoginFields
 }
-
