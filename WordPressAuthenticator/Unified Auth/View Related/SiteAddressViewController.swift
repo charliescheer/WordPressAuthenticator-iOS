54d29c8ada8c75f2c5c1bd0d05f3cc761a51f2f8
Thuy.Copeland <thuycopeland@gmail.com>
2020-05-20 17:16:53 -0500

    Outline the factory implementation for the SiteAddressViewController

--- WordPressAuthenticator/Unified Auth/View Related/SiteAddressViewController.swift
+++ WordPressAuthenticator/Unified Auth/View Related/SiteAddressViewController.swift
@@ -5,6 +5,21 @@ import UIKit
 ///
 class SiteAddressViewController: UIViewController {
 
+    typealias Factory = LoginFieldLoaderFactory & ViewControllerFactory
+
+    private let factory: Factory
+
+    private lazy var loader = factory.makeLoginFieldLoader()
+
+    init(factory: Factory) {
+        self.factory = factory
+        super.init(nibName:nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
